c954e4beac64e51480eb754f102c2335dca88914
Kuba Juszczak <kubajuszczak@gmail.com>
2018-12-10 00:14:43 +0000

    prettier printing in day 5

--- src/main/java/Day05.java
+++ src/main/java/Day05.java
@@ -3,9 +3,12 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Day05 {
     private static Path input;
+
     static {
         try {
             input = Paths.get(ClassLoader.getSystemResource("day05").toURI());
@@ -15,24 +18,23 @@ public class Day05 {
     }
 
 
-
-    private static ReactionResult react(String s){
+    private static ReactionResult react(String s) {
         int reactionCount = 0;
 
         StringBuilder builder = new StringBuilder();
-        for (int i = 1; i<s.length(); i++){
-            char a = s.charAt(i-1);
+        for (int i = 1; i < s.length(); i++) {
+            char a = s.charAt(i - 1);
             char b = s.charAt(i);
 
-            if(b>=97){
-                if(a == (b-32)){
+            if (b >= 97) {
+                if (a == (b - 32)) {
                     i++;
                     reactionCount++;
                 } else {
                     builder.append(a);
                 }
             } else {
-                if(a == (b+32)){
+                if (a == (b + 32)) {
                     i++;
                     reactionCount++;
                 } else {
@@ -40,36 +42,37 @@ public class Day05 {
                 }
             }
         }
-        builder.append(s.charAt(s.length()-1));
+        builder.append(s.charAt(s.length() - 1));
         return new ReactionResult(builder.toString(), reactionCount);
     }
 
     private static int part1(String polymer) {
         ReactionResult r = react(polymer);
-        while(r.reactionCount>0){
+        while (r.reactionCount > 0) {
             r = react(r.polymer);
         }
         return r.polymer.length();
     }
 
-    private static void part2(String s){
-        for (int i = 65; i<65+26;i++) {
-            String letter =Character.toString((char) i);
-            System.out.println(part1(s.replace(letter.toLowerCase(),"").replace(letter.toUpperCase(),"")));
+    private static int part2(String s) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 65; i < 65 + 26; i++) {
+            String letter = Character.toString((char) i);
+            set.add(part1(s.replace(letter.toLowerCase(), "").replace(letter.toUpperCase(), "")));
         }
+        return set.stream().mapToInt(Integer::intValue).min().getAsInt();
     }
 
     public static void main(String[] args) throws IOException {
         String s = Files.readAllLines(input).get(0);
 
-
-//        System.out.println(part1(s));
-        part2(s);
+        System.out.println("part 1: " + part1(s));
+        System.out.println("part 2: " + part2(s));
     }
 
 }
 
-class ReactionResult{
+class ReactionResult {
     String polymer;
     int reactionCount;
 
